a9fa00b6bb7c3a6c3f8367ba6fa8708f362b839a
Johns <william.johns@pnnl.gov>
2022-01-25 15:42:14 -0800

    Added model switching code for keeping runs scoped localy

---
+++ run_model_switch.m
@@ -0,0 +1,44 @@
+function [vorticity_full,theta_full,t_end]=run_model_switch(tF,dt,Nx,model,W,Ito_sum)
+%
+
+%% Parameters
+var_factor = 1e-6;
+ito_factor = 0;
+
+%Nx = 128*2; % x grid resolution
+T0 = 305; % surface temperature
+N02 = 1e-4; % bouyancy
+sb_factor = 0; % turn on sponge boundaries
+Amp0 = 0.01; % initial amplitude of perturbations
+kmix = 0.21;% sub-grid mixing parameter
+alpha_dc = 0.5; % de-centering
+mu = 0.0025; % diffusion parameter
+
+%% dimensions
+V = 10; % meters/second
+g = 9.8; % meters/second^2
+L = 1000; % meters
+Fr2 = V^2/(g*L); % square Froude number
+
+%% Initial State and preallocate arrays
+background_state
+initialization0
+initialization    
+clear theta_full_0 vorticity_full_0   CT_full_0
+
+%% Run the selected Model
+t_start=cputime;
+switch model
+    case 'rk2'       
+        run_model_RK2D_RK2P_sto_adv_u_2D;
+    case 'eul'
+        run_model_EulD_RK2P_sto_adv_2D
+    case 'ito'
+        run_model_EulD_RK2P_ito2D
+    case 'euleul'
+        run_model_EulD_EulP_sto_adv_2D
+    case 'itoeul'
+        run_model_EulD_EulP_ito2D
+end %end model switch
+t_end=cputime-t_start;
+end
